bcceba05f4ffb519cffce02e0037ceec35ee87a0
abdoulkarim20 <abdoulkarimfoly1996@gmail.com>
2022-11-01 20:33:41 +0000

    controle pour ajouter qu'un seul email

--- exe/etudiant/add.php
+++ exe/etudiant/add.php
@@ -1,8 +1,17 @@
 <?php
+require_once("../../dbConfig.php");
   if(!empty($_POST['nom'] && !empty($_POST['email']))){
       extract($_POST);
     //   var_dump($_POST);
-      require_once("../../dbConfig.php");
+    $sql=$dbConnexion->prepare("SELECT * FROM etudiants WHERE email=?");
+    $sql->bindParam(1,$email);
+    $sql->execute();
+    $data=$sql->fetch();
+    if($data){
+      echo "<script>alert('Cet etudiant existe deja')
+                        document.location.href='../../?r=etudiant&m=add';
+                    </script>";
+    }
     //   var_dump($dbConnexion);
       $sql=$dbConnexion->prepare("INSERT INTO etudiants(nom,email) VALUES(?,?)");
       $sql->bindParam(1,$nom);
